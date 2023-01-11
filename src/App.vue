42a42681c626a1f86c8c400122c48b5470550641
Med Amine Rhallab <rhallab.dev@gmail.com>
2023-01-10 21:24:41 -0500

    Configure init styles and variables

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { watch, onBeforeMount } from 'vue';
 import { storeToRefs } from 'pinia'
 import { useTaskManagerStore } from '@/stores/index'
+import Sidebar from './components/Sidebar.vue';
 
 const taskManager = useTaskManagerStore()
 const { listBoards } = storeToRefs(taskManager)
@@ -16,9 +17,12 @@ onBeforeMount(()=>{
 </script>
 
 <template>
-  <main>
-    <div v-for="(board, idx) in listBoards" :key="idx">
-      <p class="text-3xl font-bold underline">{{ board.name }}</p>
+  <main class="flex h-full">
+    <Sidebar />
+    <div>
+      <div v-for="(board, idx) in listBoards" :key="idx">
+        <p class="text-3xl font-bold underline">{{ board.name }}</p>
+      </div>
     </div>
   </main>
 </template>
